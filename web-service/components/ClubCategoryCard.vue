5998acdc9b65f2f3fcdf0333d77f9edd32949c63
JeonJaeYun <nick9297@naver.com>
2023-09-01 16:29:41 +0900

    test2

--- web-service/components/ClubCategoryCard.vue
+++ web-service/components/ClubCategoryCard.vue
@@ -79,11 +79,6 @@ export default {
           title: "예술",
           to: "/art",
         },
-        {
-          icon: "mdi-palette",
-          title: "테스트",
-          to: "/art",
-        },
       ],
       title: "Club Categories",
     };
